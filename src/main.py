406db143ce12714f5c6efc3d003d9e5acde67c0e
Guahao <guahao@zju.edu.cn>
2022-09-03 21:05:28 +0800

    update: get the dictionary with nets_name : [time, value]

--- src/main.py
+++ src/main.py
@@ -1,3 +1,4 @@
+import json
 import Verilog_VCD
 import sys
 import logging
@@ -36,14 +37,30 @@ def test_sigs(sigs_tester, sigs_4_test):
 
     return res, res_list
 
+def get_value_change_dict(vcd):
+    # Get vcd from the struct 
+    # return with a dict that nets_name -> [ time , value ]
+    res_dict = {}
+    for element in vcd.values():
+        nets = element['nets']
+        nets = nets[0]
+        nets_name = nets['hier'] + '.' + nets['name']
+        t_v = []
+        tv_table = element['tv']
+        for tv in tv_table:
+            t_v.append(tuple([tv[0], int(tv[1], 2)]))
+        res_dict[nets_name] = t_v
+
+    return res_dict
+
 def main():
     # The level of log, can be commented when no need to read INFO log
     logging.basicConfig(level=logging.INFO)
     file_tester, file_4_test = read_in_filename(sys.argv)
 
+    # Get the signals for tester and tested file
     sigs_tester, sigs_4_test = Verilog_VCD.list_sigs(file_tester), Verilog_VCD.list_sigs(file_4_test)
-    # print(sigs_tester)
-    # print(sigs_4_test)
+    # test if all of the signals in tester are in file tested
     test_sigs_res, test_sigs_list = test_sigs(sigs_tester, sigs_4_test)
     if not test_sigs_res:
         error_msg = 'mini_Compare_VCD: 需要将以下信号添加进被检测的vcd文件\n\t'
@@ -52,12 +69,15 @@ def main():
         logging.error(error_msg)
         exit(1)
 
+    # Get the dict that nets_name: value_changed_list
     vcd_tester = Verilog_VCD.parse_vcd(file_tester)
     timescale, endtime = Verilog_VCD.get_timescale(), Verilog_VCD.get_endtime()
-    print(timescale, endtime)
-        
-
-
+    dict_tester = get_value_change_dict(vcd_tester)
+    # print(json.dumps(vcd_tester, indent=4))
+    vcd_4_test = Verilog_VCD.parse_vcd(file_4_test)
+    dict_4_test = get_value_change_dict(vcd_4_test)
+    
+    
 
 
     
